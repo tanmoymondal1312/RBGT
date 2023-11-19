3e659896aa7ff28b0400ce3a1d91226257d66a97
TanmoyBD <tanmoymondal1315@gmail.com>
2023-11-19 19:53:01 +0600

    Complete for deploy

--- XXX/RBGT.py
+++ XXX/RBGT.py
@@ -3,7 +3,6 @@ from PIL import Image
 import numpy as np
 import os
 import uuid
-
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
@@ -12,17 +11,10 @@ def remove_background(image_name):
     img = Image.open(input_img_path)
     img_array = np.array(img)
     result = remove(img_array)
-
-    # Generate a unique name starting with "rbgt"
     unique_name = "rbgt" + str(uuid.uuid4())[:8]
-
-    # Construct the full path to the output image in the 'bg_removed_images' folder
     output_folder = r"C:\DJANGOOOOOOOOOOOOOO\MY PORTFOLIO WITH DJANGO TEMPLATES\RBGT\media\bg_removed_images"
     output_img_path = os.path.join(output_folder, f"{unique_name}.png")
-
-    # Save the processed image as PNG using Django's file handling
     with default_storage.open(output_img_path, 'wb') as file:
         processed_image = Image.fromarray(result)
         processed_image.save(file, format='PNG')
-
     return output_img_path
--- remove_bg/views.py
+++ remove_bg/views.py
@@ -9,9 +9,7 @@ from XXX.RBGT import *
 
 def index(request):
     processed_image_url = None 
-
     if not request.session.session_key:
-        print("Dhukce")
         request.session.create()
         print(request.session.session_key)
         UserSession.objects.create(session_key=request.session.session_key)
@@ -20,7 +18,6 @@ def index(request):
         form = PictureUploadForm(request.POST, request.FILES)
         if form.is_valid():
             user_session = UserSession.objects.get(session_key=request.session.session_key)
-
             original_file_name, file_extension = os.path.splitext(form.cleaned_data['image'].name)
             unique_name = f"{str(uuid.uuid4())[:8]}{file_extension}"
             image_name = original_file_name
@@ -29,29 +26,16 @@ def index(request):
             file_path = os.path.join('media', 'images', unique_name)
             with open(file_path, 'wb') as file:
                 for chunk in form.cleaned_data['image'].chunks():
-                    file.write(chunk)
-            
-            processed_image_path = remove_background(unique_name)
-            
+                    file.write(chunk)          
+            processed_image_path = remove_background(unique_name)          
             existing_image_instance = Images.objects.get(image=unique_name)
-
-            # Update the converted_image field
             existing_image_instance.converted_image = processed_image_path
-            existing_image_instance.save()
-            
+            existing_image_instance.save()    
             processed_image_url = existing_image_instance.converted_image.url
-
-            # Store processed_image_url in the session
             request.session['processed_image_url'] = processed_image_url
-    
             return redirect('index')
     else:
         form = PictureUploadForm()
-
-    # Retrieve processed_image_url from the session
     processed_image_url = request.session.get('processed_image_url')
-    request.session['processed_image_url'] = None  # Clear the session value
-
-    print("Processed Image URL outside POST block:", processed_image_url)
-            
+    request.session['processed_image_url'] = None      
     return render(request, 'index.html', {'form': form, 'processed_image_url': processed_image_url})
